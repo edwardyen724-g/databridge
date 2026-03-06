cec5088ea138e4c311a26a179908fbd2f36b8d18
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 03:58:54 -0800

    feat: add src/lib/stripe.ts

---
+++ src/lib/stripe.ts
@@ -0,0 +1,23 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2022-11-15',
+});
+
+export default stripe;
+
+export const createCheckoutSession = async (lineItems: {price: string; quantity: number}[]) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/cancel`,
+    });
+
+    return session;
+  } catch (err) {
+    throw new Error(err instanceof Error ? err.message : String(err));
+  }
+};
